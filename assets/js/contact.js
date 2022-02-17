c852b483c96882630e3901aa2de6f14ecb5fe7d3
Ewout De Coster <ewout.decoster@student.hogent.be>
2022-02-17 15:43:24 +0100

    layout fixes

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -9,19 +9,34 @@ function getUrlVars() {
   return vars;
 }
 
-function getSubject() {
-  let formule = getUrlVars().formule;
-  if (formule == undefined) {
-    return ""
-  } else {
-    return unescape(formule);
+function getSelectr() {
+  let content = document.getElementById("selectr");
+
+  if (content.value == 1) {
+    document.getElementById("msg").value =
+      "Ik had graag wat meer informatie gekregen over de opruimsessie.";
+  } else if (content.value == 2) {
+    document.getElementById("msg").value =
+      "Ik had graag wat meer informatie gekregen over het adviesgesprek.";
   }
 }
 
+function getSubject() {
+  let formule = getUrlVars().formule;
+  if (formule == undefined) {
+    return "";
+  } else {
+    return unescape(formule + "\n");
+  }
+}
 
 function init() {
-    document.getElementById("msg").value = getSubject() + "\n";
+  document.getElementById("msg").value = getSubject();
 
+  document.getElementById("selectr").onclick = (event) => {
+    event.preventDefault();
+    getSelectr();
+  };
 }
 
 window.onload = init;
